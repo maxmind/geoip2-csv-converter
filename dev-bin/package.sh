289da588f6b9ce30e191efeae1748695f7073169
Gregory Oschwald <goschwald@maxmind.com>
2014-12-09 15:54:24 -0800

    Include arch in package name

--- dev-bin/package.sh
+++ dev-bin/package.sh
@@ -16,19 +16,18 @@ zip "geoip2-csv-converter-${VERSION}-windows-64.zip" geoip2-csv-converter.exe \
 
 rm -f geoip2-csv-converter.exe
 
+ARCH=amd64
 for OS in linux darwin
 do
     echo -e "\nBuilding for $OS"
     DIR="geoip2-csv-converter-${VERSION}"
     mkdir $DIR
-    GOOS=$OS GOARCH=amd64 go build
+    GOOS=$OS GOARCH=$ARCH go build
     mv geoip2-csv-converter $DIR
     cp README.md LICENSE $DIR
-    tar cfvz "${DIR}-${OS}.tar.gz" $DIR
+    tar cfvz "${DIR}-${OS}-${ARCH}.tar.gz" $DIR
     rm -r $DIR
 done
-
-rm -f geoip2-csv-converter
 
 git tag -a $VERSION
 git push -u --tags
