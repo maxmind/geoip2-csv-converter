fa13cf95793536fc3a4a80ba96d5482b85502523
Gregory Oschwald <goschwald@maxmind.com>
2018-12-05 08:40:00 -0800

    Try to improve error output when require flag is missing

--- main.go
+++ main.go
@@ -3,38 +3,48 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/maxmind/geoip2-csv-converter/convert"
 )
 
 func main() {
-	required := "REQUIRED"
-	input := flag.String("block-file", required, "The path to the block CSV file to use as input")
-	output := flag.String("output-file", required, "The path to the output CSV")
+	input := flag.String("block-file", "", "The path to the block CSV file to use as input (REQUIRED)")
+	output := flag.String("output-file", "", "The path to the output CSV (REQUIRED)")
 	ipRange := flag.Bool("include-range", false, "Include the IP range of the network in string format")
 	intRange := flag.Bool("include-integer-range", false, "Include the IP range of the network in integer format")
 	cidr := flag.Bool("include-cidr", false, "Include the network in CIDR format")
 
 	flag.Parse()
 
-	if *input == required || *output == required {
-		printHelp()
-		return
+	var errors []string
+
+	if *input == "" {
+		errors = append(errors, "-block-file is required")
+	}
+
+	if *output == "" {
+		errors = append(errors, "-output-file is required")
 	}
 
 	if !*ipRange && !*intRange && !*cidr {
-		printHelp()
-		return
+		errors = append(errors, "At least one of -include-* param is required")
+	}
+
+	if len(errors) != 0 {
+		printHelp(errors)
+		os.Exit(1)
 	}
 
 	err := convert.ConvertFile(*input, *output, *cidr, *ipRange, *intRange)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Fprintf(flag.CommandLine.Output(), "Error: %v\n", err)
 	}
 }
 
-func printHelp() {
+func printHelp(errors []string) {
+	for _, message := range errors {
+		fmt.Fprintln(flag.CommandLine.Output(), message)
+	}
 	flag.PrintDefaults()
-	fmt.Println("\nAt least one of -include-* param is required")
-
 }
